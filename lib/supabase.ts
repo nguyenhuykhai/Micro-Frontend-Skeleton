50de6149552a815127617983c81036a66b97c3fd
Kai Nguyen <kai.nguyen@terralogic.com>
2025-11-16 16:07:43 +0700

    fix: redirect login

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      detectSessionInUrl: true,
+      flowType: 'pkce',
+      // ❌ don't set redirectTo here to localhost
+    },
+  },
+);
 
 // Database Types
 export interface Database {
